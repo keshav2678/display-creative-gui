4ad827bc798b0b6e560c26abf08a2624e6389ad0
keshav-mfilter <mfilter@keshavs-MacBook-Air.local>
2024-07-26 11:11:28 +0530

    packages fix

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -4,11 +4,13 @@ import { PackageContext } from '../../context/PackageContext';
 import { FaImage, FaListAlt } from 'react-icons/fa';
 import { FaUpload } from "react-icons/fa6";
 import { TbLayoutSidebarLeftCollapse, TbLayoutSidebarRightCollapse } from 'react-icons/tb';
+import axios from 'axios';
 import './Sidebar.css';
 
 const Sidebar = () => {
   const { selectedPackage, setSelectedPackage } = useContext(PackageContext);
   const [collapsed, setCollapsed] = useState(false);
+  const [packages, setPackages] = useState([]);
 
   const handlePackageChange = (e) => {
     setSelectedPackage(e.target.value);
@@ -17,13 +19,26 @@ const Sidebar = () => {
   const toggleSidebar = () => {
     setCollapsed(!collapsed);
   };
+
   useEffect(() => {
+    // Fetch packages from the API
+    const fetchPackages = async () => {
+      try {
+        const response = await axios.get('https://qt8flde415.execute-api.ap-south-1.amazonaws.com/get_packages');
+        setPackages(response.data);
+      } catch (error) {
+        console.error("Error fetching packages:", error);
+      }
+    };
+
+    fetchPackages();
+
     // Initialize selected package from localStorage on component mount
     const storedPackage = localStorage.getItem('selectedPackage');
     if (storedPackage) {
       setSelectedPackage(storedPackage);
     }
-     // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
@@ -35,24 +50,24 @@ const Sidebar = () => {
           onChange={handlePackageChange}
         >
           <option value="">Select Package</option>
-          <option value="package_1">Package 1</option>
-          <option value="package_2">Package 2</option>
-          <option value="package_3">Package 3</option>
+          {packages.map(pkg => (
+            <option key={pkg.id} value={pkg.id}>{pkg.package_name}</option>
+          ))}
         </select>
       </div>
       <ul className="menu-items">
         <li>
-          <Link to="/" className={!selectedPackage ? 'disabled-link' : ''} style={{justifyContent:collapsed?"center":""}} onClick={(e) => !selectedPackage && e.preventDefault()}>
-            {collapsed ? <FaUpload className="icon"  /> : <><FaUpload className="icon" /> Upload Creatives</>}
+          <Link to="/" className={!selectedPackage ? 'disabled-link' : ''} style={{ justifyContent: collapsed ? "center" : "" }} onClick={(e) => !selectedPackage && e.preventDefault()}>
+            {collapsed ? <FaUpload className="icon" /> : <><FaUpload className="icon" /> Upload Creatives</>}
           </Link>
         </li>
         <li>
-          <Link to="/creatives" className={!selectedPackage ? 'disabled-link' : ''} style={{justifyContent:collapsed?"center":""}} onClick={(e) => !selectedPackage && e.preventDefault()}>
+          <Link to="/creatives" className={!selectedPackage ? 'disabled-link' : ''} style={{ justifyContent: collapsed ? "center" : "" }} onClick={(e) => !selectedPackage && e.preventDefault()}>
             {collapsed ? <FaImage className="icon" /> : <><FaImage className="icon" /> Creatives List</>}
           </Link>
         </li>
         <li>
-          <Link to="/trackers" className={!selectedPackage ? 'disabled-link' : ''} style={{justifyContent:collapsed?"center":""}} onClick={(e) => !selectedPackage && e.preventDefault()}>
+          <Link to="/trackers" className={!selectedPackage ? 'disabled-link' : ''} style={{ justifyContent: collapsed ? "center" : "" }} onClick={(e) => !selectedPackage && e.preventDefault()}>
             {collapsed ? <FaListAlt className="icon" /> : <><FaListAlt className="icon" /> Trackers</>}
           </Link>
         </li>
